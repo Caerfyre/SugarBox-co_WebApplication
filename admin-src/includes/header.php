379f1fe78d2f54087da25323ba2652bb83770d1a
Caerfyre <ninathereseybas@gmail.com>
2022-01-15 22:27:52 +0800

    added css file

--- admin-src/includes/header.php
+++ admin-src/includes/header.php
@@ -20,6 +20,7 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.css" rel="stylesheet">
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
     <style>
         .pull-up {
