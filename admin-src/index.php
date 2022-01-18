6e82c42f49534fa879f48823f9e4695add235842
Caerfyre <ninathereseybas@gmail.com>
2022-01-18 16:21:22 +0800

    added heading

--- admin-src/index.php
+++ admin-src/index.php
@@ -8,6 +8,10 @@ include 'includes/topbar.php'
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    </div>
 
     <!-- Content Row -->
     <div class="row">
