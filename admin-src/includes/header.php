4f93ac76f15f6580908858c94d924376d52d47ae
Caerfyre <ninathereseybas@gmail.com>
2022-02-20 04:13:54 +0800

    added CSS style

--- admin-src/includes/header.php
+++ admin-src/includes/header.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styleimg.css">
 
     <style>
         html, body {
