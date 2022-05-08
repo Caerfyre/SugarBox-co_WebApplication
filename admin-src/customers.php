4c61a500a2fa7026f08111809922b1ce46795f09
Caerfyre <ninathereseybas@gmail.com>
2022-05-08 17:30:13 +0800

    Added View details content

--- admin-src/customers.php
+++ admin-src/customers.php
@@ -22,8 +22,8 @@ include 'includes/topbar.php'
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between bg-section border-section">
                     <h6 class="m-0 font-weight-bold text-content"><b>Customers List</b></h6>
                     <div>
-                    <button type="button" class="btn btn-titleColor" data-toggle="modal" data-target="#addCateg">
-                    Add Category
+                    <button type="button" class="btn btn-titleColor" data-toggle="modal" data-target="#addCustomer">
+                    Add Customer
                     </button>
                        <a href="products.php" class="btn btn-subheading">&larr; Go Back</a>  
                     </div>
@@ -97,7 +97,7 @@ include 'includes/topbar.php'
                                         <td class="font-weight-bold <?php echo $style ?>"><?php echo $status ?></td>
                                         <td class="d-sm-flex align-items-center justify-content-center">
                                         <!-- <a href="productDetails.php?prod_ID=<?php echo $row['Account_ID'];?>" class="btn btn-subheading">View Details</a> -->
-                                        <button class="btn btn-danger" data-toggle="modal" data-target="#viewDetails<?php echo $row['Account_ID'];?>">View Details</button>
+                                        <button class="btn btn-subheading" data-toggle="modal" data-target="#viewDetails<?php echo $row['Account_ID'];?>">View Details</button>
                                         &nbsp;
                                         <button class="btn btn-danger" data-toggle="modal" data-target="#deleteProd<?php echo $row['Account_ID'];?>">Delete Product</button>
                                         </td>
@@ -108,14 +108,58 @@ include 'includes/topbar.php'
                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                         <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title text-subheading" id="viewDetails"><b>Customer Details</b></h5>
+                                            <h5 class="modal-title text-subheading" id="viewDetails"><b>Customer #<?php echo $row['Account_ID'];?> Details</b></h5>
                                             <button type="button" class="close text-dark" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
 
-                                        fhhhfhgg
-                                    
+                                        <?php 
+                                        $conn = db_connect();
+                                        $acc_ID = $row['Account_ID'];
+                                        $cust_query = "SELECT accounts.Date_Created,
+                                                              customer.Cust_ContactNo,
+                                                              customer.Cust_Address
+                                                        FROM accounts
+                                                        INNER JOIN customer
+                                                        ON accounts.Account_ID = customer.Cust_ID
+                                                        WHERE accounts.Account_ID = $acc_ID";
+                
+                                        $cust_query_run = mysqli_query($conn, $cust_query);
+                                        $row2 = mysqli_fetch_assoc($cust_query_run)
+                                        ?>
+
+                                        <div class="modal-body">
+                                            <div class="text-center">
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2 text-right"><b class="font-weight-bolder">Customer ID:</b></p></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $row['Account_ID'];?></p></div>
+                                                    </div>
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2"><b class="font-weight-bolder">Username:</b></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $row['Acc_Username'];?></p></div>
+                                                    </div>
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2"><b class="font-weight-bolder">Status:</b></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $status;?></p></div>
+                                                    </div> 
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2"><b class="font-weight-bolder">Customer Name:</b></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $row['Cust_FName'];?> <?php echo $row['Cust_LName']; ?></p></div>
+                                                    </div> 
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2"><b class="font-weight-bolder">Contact Number:</b></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $row2['Cust_ContactNo'];?></p></div>
+                                                    </div>    
+                                                    <div class="row justify-content-center">
+                                                        <div class="col text-right"><p class="text-content font-weight-bold mb-2"><b class="font-weight-bolder">Address:</b></div>
+                                                        <div class="col text-left"><p class="text-content font-weight-bold mb-2"><?php echo $row2['Cust_Address'];?></p></div>
+                                                    </div>   
+                                                </div>
+
+                                            </div>             
+                                        </div>  
+
                                         </div>
                                     </div>
                                     </div>
